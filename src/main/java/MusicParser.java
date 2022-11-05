d3d32d8f02dee2a09087282f596ba435a2dedaf5
scroogemcfawk <scroogemcfawk@gmail.com>
2022-11-05 16:55:36 +0700

    New: Video parser v0.0.2

--- src/main/java/MusicParser.java
+++ src/main/java/MusicParser.java
@@ -22,7 +22,6 @@ public class MusicParser
         url = url.strip();
         Connection.Response res = Jsoup.connect(url).followRedirects(true).execute();
         url = res.url().toString();
-        lastURL = url;
 
         if (url.matches("(?:http[s]?:\\/\\/)?(?:www.)?(?:[a-z]*\\.)*tiktok\\.com\\/.*music\\/.+")) {
             return parseMusic(url);
@@ -40,30 +39,17 @@ public class MusicParser
     }
 
     private static String parseMusic(String url) {
+        lastURL = url;
         try {
             Document doc = Jsoup.connect(url).get();
             var jsonResponse = doc.select("#SIGI_STATE").html();
-
-//            System.out.println(jsonResponse);
+            logger.info(jsonResponse);
 
             var obj = new JSONObject(jsonResponse);
-            System.out.println(obj.length());
+
             var music = obj.getJSONObject("MusicModule").getJSONObject("musicInfo").getJSONObject("music");
             String musicURL = music.getString("playUrl");
             System.out.println(musicURL);
-//            var arr = new JSONArray(obj.toString());
-//            arr.getJSONArray();
-//            System.out.println(arr.toString());
-//            for (int i = 0; i < arr.length(); i++) { // Walk through the Array.
-//                JSONObject obj = arr.getJSONObject(i);
-//                System.out.println(obj.toString());
-//            }
-
-
-
-//            System.out.println(music.toString());
-
-//            System.out.println(doc.html());
             return "";
         }
         catch (IOException e)
@@ -72,14 +58,38 @@ public class MusicParser
         }
     }
 
+
+    // TODO: refactor this shit
     private static String parseVideo(String url) {
-        return null;
+        lastURL = url;
+        try {
+            Document doc = Jsoup.connect(url).get();
+            var jsonResponse = doc.select("#SIGI_STATE").html();
+//            logger.info(jsonResponse);
+
+            var obj = new JSONObject(jsonResponse);
+
+//            var music = obj.getJSONObject("ItemModule").getJSONObject(0).getJSONObject("music");
+            var module = obj.getJSONObject("ItemModule");
+            var music = module.getJSONObject(module.keys().next()).getJSONObject("music");
+
+            String musicURL = music.getString("playUrl");
+            System.out.println(musicURL);
+//            logger.info(music.toString());
+            return "";
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) throws Exception
     {
-        System.out.println(MusicParser.parse("https://vt.tiktok.com/ZSR7kQNUr/"));
-        System.out.println(lastURL);
+        parseVideo("https://www.tiktok.com/@genboy3/video/7075486642357718298?_r=1&_t=8X0xGtbsjXF&is_from_webapp=v1" +
+                   "&item_id=7075486642357718298");
+//        System.out.println(MusicParser.parse("https://vt.tiktok.com/ZSR7kQNUr/"));
+//        System.out.println(lastURL);
     }
 
 }
