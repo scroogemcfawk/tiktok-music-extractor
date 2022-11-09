735f1257bf345cbb930d341da366b6e8c022f29a
scroogemcfawk <scroogemcfawk@gmail.com>
2022-11-09 12:45:26 +0700

    Update: MusicParser refactor v0.0.4

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@
  * @version v0.0.3
  * @since v0.0.1
  */
-
+// TODo: добавить yeild() в threadpool, сделать человеческий Singleton как тут https://habr.com/ru/post/129494/
 public class App
 {
 
--- src/main/java/MusicParser.java
+++ src/main/java/MusicParser.java
@@ -1,6 +1,5 @@
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.slf4j.*;
 
 import org.json.*;
@@ -8,28 +7,23 @@ import org.json.*;
 import java.io.IOException;
 
 /**
- * This huita is working a lil bit
+ * Gets the URL of music source from given TikTok video or music URL.
+ *
  * @since v0.0.1
  */
 public class MusicParser
 {
     private static final Logger logger = LoggerFactory.getLogger(MusicParser.class);
 
-    public static String lastURL = "";
-
-    public static String parse(String url) throws Exception
+    public static String get(String url) throws Exception
     {
-        url = url.strip();
-        Connection.Response res = Jsoup.connect(url).followRedirects(true).execute();
-        url = res.url().toString();
+        String goal = url.strip();
+        Connection.Response res = Jsoup.connect(goal).followRedirects(true).execute();
+        goal = res.url().toString();
 
-        if (url.matches("(?:http[s]?:\\/\\/)?(?:www.)?(?:[a-z]*\\.)*tiktok\\.com\\/.*music\\/.+")) {
-            return parseMusic(url);
-        }
-        else if (url.matches("(?:http[s]?:\\/\\/)?(?:www.)?(?:[a-z]*\\.)*tiktok\\.com\\/.*video\\/.+"))
+        if (goal.matches("(?:http[s]?://)?(?:www.)?(?:[a-z]*\\.)*tiktok\\.com/.*.{5}/.+"))
         {
-            logger.info("" + url);
-            return parseVideo(url);
+            return parse(url);
         }
         else
         {
@@ -38,45 +32,19 @@ public class MusicParser
         }
     }
 
-    private static String parseMusic(String url) {
-        lastURL = url;
-        try {
-            Document doc = Jsoup.connect(url).get();
-            var jsonResponse = doc.select("#SIGI_STATE").html();
-            logger.info(jsonResponse);
-
-            var obj = new JSONObject(jsonResponse);
-
-            var music = obj.getJSONObject("MusicModule").getJSONObject("musicInfo").getJSONObject("music");
-            String musicURL = music.getString("playUrl");
-            System.out.println(musicURL);
-            return "";
-        }
-        catch (IOException e)
+    // Works for both music and video URLs, not sure if there is a faster way of getting the source URL
+    private static String parse(String url)
+    {
+        try
         {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    // TODO: refactor this shit
-    private static String parseVideo(String url) {
-        lastURL = url;
-        try {
             Document doc = Jsoup.connect(url).get();
-            var jsonResponse = doc.select("#SIGI_STATE").html();
-//            logger.info(jsonResponse);
-
-            var obj = new JSONObject(jsonResponse);
-
-//            var music = obj.getJSONObject("ItemModule").getJSONObject(0).getJSONObject("music");
-            var module = obj.getJSONObject("ItemModule");
+            // Get JSON obj in HTML doc
+            var jsonElement = doc.select("#SIGI_STATE").html();
+            var jsonObject = new JSONObject(jsonElement);
+            var module = jsonObject.getJSONObject("ItemModule");
             var music = module.getJSONObject(module.keys().next()).getJSONObject("music");
-
-            String musicURL = music.getString("playUrl");
-            System.out.println(musicURL);
-//            logger.info(music.toString());
-            return "";
+            // Get music source URL
+            return music.getString("playUrl");
         }
         catch (IOException e)
         {
@@ -86,10 +54,14 @@ public class MusicParser
 
     public static void main(String[] args) throws Exception
     {
-        parseVideo("https://www.tiktok.com/@genboy3/video/7075486642357718298?_r=1&_t=8X0xGtbsjXF&is_from_webapp=v1" +
-                   "&item_id=7075486642357718298");
-//        System.out.println(MusicParser.parse("https://vt.tiktok.com/ZSR7kQNUr/"));
-//        System.out.println(lastURL);
+        String url = "https://www.tiktok.com/music/Cool-for-the-Summer-Sped-Up-Nightcore-7067574132983991045?_d" +
+                     "=secCgYIASAHKAESPgo8iTlnQLKqD5%2BYzJDtDmT%2FymdUjZvvV7ymamczyiQdIVsMecyTVMLRG68RM%2BD" +
+                     "%2FdomrkMCiWb6Oi1MSq1PaGgA%3D&_r=1&language=en&sec_user_id" +
+                     "=MS4wLjABAAAA7__kURiSKYxLyDuB4cnzZ_sIABe8XIxjw3K9R0c3KLwnEwLdk06ZKbNtG2Jet9Cs&share_app_id=1233" +
+                     "&share_link_id=b2476394-207f-4547-aa4d-2e05b9cb776b&share_music_id=7067574132983991045&source" +
+                     "=h5_m" +
+                     "&timestamp=1667374361&u_code=dmabmlfkcef9h9&ugbiz_name=Unknown&user_id=7044619555884696578" +
+                     "&utm_campaign=client_share&utm_medium=android&utm_source=telegram";
+        System.out.println(get(url));
     }
-
 }
